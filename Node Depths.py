bd2dd289491da3d77edea54a88e60c91d0600f08
Eswar Kumar <eswar.bade62@gmail.com>
2021-10-17 18:15:59 +0530

    Update Node Depths.py

--- Node Depths.py
+++ Node Depths.py
@@ -1,19 +1,21 @@
 """"
-            1
+            1                             0- Depth
         /       \
-       2         3
+       2         3                        1- Depth 
      /  \      /   \
-    4    5    6     7
+    4    5    6     7                     2- Depth
   /  \
-8     9
+8     9                                   3- Depth
 
 
 
 
-# Depth of a node is the distance between the respective node to the root node
-# This can be solved by using either recursive method or iterative method.
-# Time: O(n) and Space: O(h) 'h' is height
+Depth of a node is the distance between the respective node to the root node
+This can be solved by using either recursive method or iterative method.
+Time: O(n) and Space: O(h) 'h' is height
 
+# Output returns Sum of depths: 16
+(0+1+1+2+2+2+2+3+3) 
 """
 
 
@@ -33,7 +35,7 @@ def nodeDepths(root):
         stack.append({'node': node.left, 'depth': depth+1})
         stack.append({'node': node.right, 'depth': depth + 1})
     return sumOfDepths
-
+# Output returns 16(0+1+1+2+2+2+2+3+3)
 #-------------------------------------------------
 # Using recursive method
 # Time: O(n) and Space: O(h) 'h' is height
@@ -43,3 +45,5 @@ def nodeDepths(root, depth = 0):
     if root is None:
         return 0
     return depth + nodeDepths(root.left, depth+1), nodeDepths(root.right, depth+1)
+            # recursively calls left and right sides of each node while updating depths until it reaches None
+# Output returns 16(0+1+1+2+2+2+2+3+3)
